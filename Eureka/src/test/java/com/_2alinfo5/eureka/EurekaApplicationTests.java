e0566e9f114b75a3f7468c0a08517c65e3147bf1
Firas Fejjeri <126566428+NapsterFactory@users.noreply.github.com>
2024-06-26 10:26:18 +0100

    added eureka module

---
+++ Eureka/src/test/java/com/_2alinfo5/eureka/EurekaApplicationTests.java
@@ -0,0 +1,15 @@
+package com._2alinfo5.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
