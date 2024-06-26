e27f08d6aeafb2ac92b4cd44f75b6b28cdeb4530
Firas Fejjeri <126566428+NapsterFactory@users.noreply.github.com>
2024-06-26 09:02:57 +0100

    initial commit - MS project

---
+++ src/main/java/com/_2alinfo5/validationmicroservice/ValidationMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com._2alinfo5.validationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationMicroServiceApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run(ValidationMicroServiceApplication.class , args);
+    }
+
+}
---
+++ src/test/java/com/_2alinfo5/validationmicroservice/ValidationMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com._2alinfo5.validationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
